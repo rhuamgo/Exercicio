a8559965c11dc5c28ef3261590534b1b66c6b0c6
RhuamG <106784443+rhuamgo@users.noreply.github.com>
2024-03-28 21:22:16 -0300

    Add files via upload

---
+++ contar de 0 a 10 while.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+ //Faça um programa que imprima os números de 1 até 10
+int main()
+{
+    int numero= 0;
+    
+    while (numero <=10){
+        printf("%i \n", numero);
+        numero ++;
+        
+    }
+   
+}
