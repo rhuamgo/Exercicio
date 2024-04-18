ff5fb1e40537c7f9f68d8d7aa61f476c91e13590
RhuamG <106784443+rhuamgo@users.noreply.github.com>
2024-04-18 20:00:14 -0300

    Add files via upload

---
+++ contador_1a10.c
@@ -0,0 +1,20 @@
+
+// Exercício 1- Faça um programa que imprima os números de 1 até 10 (ordem crescente). 
+ 
+#include <stdio.h>
+
+int main()
+{
+  //Variaveis
+ int i = 0;
+ int contador = 1;
+ 
+ while(i <= 9)
+ {
+     printf("%i\n", contador);
+
+i++;
+contador ++;
+ }
+   
+}
