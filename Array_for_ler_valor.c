946db588e69e9798208a8e52af4be091d6fa55d9
RhuamG <106784443+rhuamgo@users.noreply.github.com>
2024-05-09 20:10:27 -0300

    Add files via upload

---
+++ Array_for_ler_valor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+//Faça um Programa que leia um vetor de 5 números e exiba na tela
+
+int main()
+{
+int recebevalor[5] ={0,0,0,0,0};
+int i;
+
+  
+  for(i=0;i <5; i++){
+    printf("Digite os valores: ");
+    scanf("%i", &recebevalor[i]);
+   
+}
+printf(" \n");
+   for(i=0; i < 5; i++){
+        printf("Esses foram os valores digitados: %i \n", recebevalor[i]); 
+   }
+   
+   
+}
